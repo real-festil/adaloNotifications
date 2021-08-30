76ace39d374bb1018037003f0bdee81701e4c928
Roman Gladyshev <romagladyshev@gmail.com>
2021-08-30 16:10:26 +0600

    init commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private schedulerRegistry: SchedulerRegistry,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  createNotification(
+    @Body()
+    body: {
+      date: string;
+      appId: string;
+      email: string;
+      titleText: string;
+      bodyText: string;
+    },
+  ): any {
+    console.log(`body`, body);
+    return this.appService.createNotification(
+      body.appId,
+      body.email,
+      body.titleText,
+      body.bodyText,
+      body.date,
+      this.schedulerRegistry,
+    );
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import axios from 'axios';
+import { CronJob } from 'cron';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  createNotification(
+    appId: string,
+    email: string,
+    titleText: string,
+    bodyText: string,
+    date: string,
+    schedulerRegistry: SchedulerRegistry,
+  ) {
+    if (email && appId && date) {
+      const job = new CronJob(new Date(date), () => {
+        console.log(`time (${email}) for job ${email} to run!`);
+        axios.post('https://api.adalo.com/notifications', {
+          appId: appId,
+          audience: { email: email },
+          notification: {
+            titleText: titleText,
+            bodyText: bodyText,
+          },
+        });
+      });
+
+      schedulerRegistry.addCronJob(email + JSON.stringify(date), job);
+      job.start();
+      console.log(
+        `job ${email + JSON.stringify(date)} added for ${date} date!`,
+      );
+    }
   }
 }
