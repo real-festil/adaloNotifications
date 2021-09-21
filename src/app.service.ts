611f9fd252f4d83a19c652711560b0aba607b79d
Roman Gladyshev <romagladyshev@gmail.com>
2021-09-21 19:08:58 +0600

    add rows deletion

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,7 @@ import { CronJob } from 'cron';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Notification } from './notification.entity';
+import * as moment from 'moment';
 
 @Injectable()
 export class AppService implements OnModuleInit {
@@ -14,18 +15,60 @@ export class AppService implements OnModuleInit {
     private schedulerRegistry: SchedulerRegistry,
   ) {}
 
-  async onModuleInit() {
-    const notifications = this.notificationsRepository.find();
-    (await notifications).forEach((notification) => {
-      this.createNotification(
-        notification.appId,
-        notification.email,
-        notification.titleText,
-        notification.bodyText,
-        notification.date,
-        this.schedulerRegistry,
-        notification.token,
+  createCronJob(
+    appId,
+    email,
+    titleText,
+    bodyText,
+    date,
+    schedulerRegistry,
+    token,
+  ) {
+    const job = new CronJob(new Date(date), async () => {
+      console.log(`time (${email}) for job ${email} to run!`);
+      const res = await axios.post(
+        'https://api.adalo.com/notifications',
+        {
+          appId: appId,
+          audience: { email: email },
+          notification: {
+            titleText: titleText,
+            bodyText: bodyText,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
       );
+      console.log('Adalo res:', res);
+    });
+
+    schedulerRegistry.addCronJob(email + JSON.stringify(date), job);
+    job.start();
+    console.log(`job ${email + JSON.stringify(date)} added for ${date} date!`);
+    console.log(
+      `jobs list: ${JSON.stringify(schedulerRegistry.getCronJobs())}`,
+    );
+  }
+
+  async onModuleInit() {
+    const notifications = await this.notificationsRepository.find();
+    notifications.forEach(async (notification) => {
+      if (moment(notification.date).isAfter(new Date())) {
+        this.createCronJob(
+          notification.appId,
+          notification.email,
+          notification.titleText,
+          notification.bodyText,
+          notification.date,
+          this.schedulerRegistry,
+          notification.token,
+        );
+      } else {
+        await this.notificationsRepository.delete(notification.date);
+      }
     });
   }
 
@@ -47,34 +90,14 @@ export class AppService implements OnModuleInit {
         date,
         token,
       });
-      const job = new CronJob(new Date(date), async () => {
-        console.log(`time (${email}) for job ${email} to run!`);
-        const res = await axios.post(
-          'https://api.adalo.com/notifications',
-          {
-            appId: appId,
-            audience: { email: email },
-            notification: {
-              titleText: titleText,
-              bodyText: bodyText,
-            },
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
-        );
-        console.log('Adalo res:', res);
-      });
-
-      schedulerRegistry.addCronJob(email + JSON.stringify(date), job);
-      job.start();
-      console.log(
-        `job ${email + JSON.stringify(date)} added for ${date} date!`,
-      );
-      console.log(
-        `jobs list: ${JSON.stringify(schedulerRegistry.getCronJobs())}`,
+      this.createCronJob(
+        appId,
+        email,
+        titleText,
+        bodyText,
+        date,
+        schedulerRegistry,
+        token,
       );
     }
   }
